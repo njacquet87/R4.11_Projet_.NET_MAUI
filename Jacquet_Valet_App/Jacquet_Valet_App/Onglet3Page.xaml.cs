63f65df1b94d70da79db9c13442af933e0c988ca
Noé Jacquet <noe.jacquet87@gmail.com>
2026-03-29 21:30:48 +0200

    add UI to add a film

--- Jacquet_Valet_App/Jacquet_Valet_App/Onglet3Page.xaml.cs
+++ Jacquet_Valet_App/Jacquet_Valet_App/Onglet3Page.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Jacquet_Valet_App.Models;
 
 namespace Jacquet_Valet_App;
 
@@ -11,5 +12,20 @@ public partial class Onglet3Page : ContentPage
     public Onglet3Page()
     {
         InitializeComponent();
+    }
+
+    private async void OnPickPhotoButtonClicked(object sender, EventArgs e)
+    {
+        // TODO 
+    }
+
+    private async void OnTakePhotoButtonClicked(object sender, EventArgs e)
+    {
+        // TODO
+    }
+
+    private void OnAddMovieButtonClicked(object sender, EventArgs e)
+    {
+        // TODO
     }
 }
